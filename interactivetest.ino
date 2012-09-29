e676fcec1820c97e9b34bc9fb3a3e525486ac89c
John Wilson <crazybutable@gmail>
2012-09-28 22:28:35 -0500

    Made the strip colors be variables instead of magic numbers

--- interactivetest.ino
+++ interactivetest.ino
@@ -13,35 +13,34 @@ const int clockPin = 3;    // Green wire on Adafruit Pixels
 // Don't forget to connect the ground wire to Arduino ground,
 // and the +5V wire to a +5V supply
 
-// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
+// Set the first variable to the NUMBER of pixels in your strip.
 
 const int numberOfPixels = 25;
 const int inputPin = A0;
 
 Adafruit_WS2801 strip = Adafruit_WS2801(numberOfPixels, dataPin, clockPin);
 
+uint32_t backgroundColor = Color(0, 255, 255);
+uint32_t movingPixelColor = Color(255, 0, 0);
+
 void setup() {
 
   strip.begin();
-
   pinMode(inputPin, INPUT);
 
-  // Update LED contents, to start they are all 'off'
-  strip.show();
 }
 
-
 void loop() {
-  
- int input = analogRead(inputPin);
+
+  int input = analogRead(inputPin);
 
   for(int i = 0; i < numberOfPixels; i++)
   {
-    strip.setPixelColor(i, Color(0, 255,255));
+    strip.setPixelColor(i, backgroundColor);
   }
 
-  strip.setPixelColor(map(input, 0, 1023, 0, numberOfPixels-1), Color(255, 0, 0));
-  
+  strip.setPixelColor(map(input, 0, 1023, 0, numberOfPixels-1), movingPixelColor);
+
   strip.show();
 }
 
@@ -58,4 +57,5 @@ uint32_t Color(byte r, byte g, byte b)
   c |= b;
   return c;
 }
+
 
